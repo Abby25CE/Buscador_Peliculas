f453f7166e15d8fb7be1410a7530a0535fd64755
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2024-07-07 21:05:53 -0600

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,11 @@
 function ListOfMovies({ movies }) {
   return (
-    <ul className="grid grid-cols-5 gap-5">
+    <ul className="grid items-start justify-center grid-cols-6 gap-5 ">
       {movies.map((movie) => (
-        <li className="" key={movie.id}>
+        <li className="text-center " key={movie.id}>
           <h3>{movie.title}</h3>
           <p>{movie.year}</p>
-          <img src={movie.poster} alt={movie.Title} width={250} />
+          <img className="" src={movie.poster} alt={movie.Title} width={200} />
         </li>
       ))}
     </ul>
